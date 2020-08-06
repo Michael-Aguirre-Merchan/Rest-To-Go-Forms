3ea9d3057b0295f4d8e8537197704637ac60f41a
Canuto2 <maikol.merchan23@gmail.com>
2020-08-05 19:36:32 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddProductsComponent,
+    CategoriesComponent,
+    AddToCartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
